e9639acb3b69e71f96e135556ee289d58149ec60
Tarasov Oleg <tarasovom@mail.ru>
2024-09-18 14:15:42 +0300

    initial

---
+++ src/main/java/org/example/service/PostService.java
@@ -0,0 +1,31 @@
+package org.example.service;
+import org.example.repository.PostRepository;
+import org.example.exception.NotFoundException;
+import org.example.model.Post;
+import java.util.Collection;
+
+
+public class PostService {
+    private final PostRepository repository;
+
+    public PostService(PostRepository repository) {
+        this.repository = repository;
+    }
+
+    public Collection<Post> all() {
+        return repository.all();
+    }
+
+    public Post getById(long id) {
+        return repository.getById(id).orElseThrow(NotFoundException::new);
+    }
+
+    public Post save(Post post) {
+        return repository.save(post);
+    }
+
+    public void removeById(long id) {
+        repository.removeById(id);
+    }
+}
+
